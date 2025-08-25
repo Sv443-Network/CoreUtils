4c4510beb7dfae0a382dba59ba62b79f8fa02118
Sv443 <contact@sv443.net>
2025-08-25 21:06:04 +0200

    feat: new error classes ScriptContextError & NetworkError

--- lib/test/Errors.spec.ts
+++ lib/test/Errors.spec.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from "vitest";
-import { ChecksumMismatchError, CustomError, DatedError, MigrationError, ValidationError } from "../Errors.js";
+import { ChecksumMismatchError, CustomError, DatedError, MigrationError, NetworkError, ScriptContextError, ValidationError } from "../Errors.js";
 import { valsWithin } from "../math.js";
 
 describe("Errors", () => {
@@ -7,21 +7,27 @@ describe("Errors", () => {
     const classes = [
       ["DatedError", "class", DatedError],
       ["ChecksumMismatchError", "class", ChecksumMismatchError],
-      ["TestCustomError", "fn", (msg: string) => new CustomError("TestCustomError", msg), CustomError],
+      ["TestCustomError", "fn", (msg: string, opts?: ErrorOptions) => new CustomError("TestCustomError", msg, opts), CustomError],
       ["MigrationError", "class", MigrationError],
       ["ValidationError", "class", ValidationError],
+      ["ScriptContextError", "class", ScriptContextError],
+      ["NetworkError", "class", NetworkError],
     ] as const;
     const errTime = Date.now();
 
     for(const [name, type, Cls, OrigCls] of classes) {
+      const errName = `Test ${name}`;
+      const errOpts: ErrorOptions = { cause: new CustomError("InnerError", "x") };
+
       const instance = type === "fn"
-        ? Cls(`Test ${name}`)
-        : new Cls(`Test ${name}`);
+        ? Cls(errName, errOpts)
+        : new Cls(errName, errOpts);
 
       expect(instance).toBeInstanceOf(type === "class" ? Cls : OrigCls);
       expect(instance.name).toBe(name);
       expect(instance.message).toBe(`Test ${name}`);
       expect(instance.date).toBeInstanceOf(Date);
+      expect(instance.cause).toBe(errOpts.cause);
       expect(valsWithin(errTime, instance.date.getTime(), undefined, 50)).toBe(true);
     }
   });
